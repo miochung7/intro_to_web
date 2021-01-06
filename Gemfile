34ca77aaff2533ecd0497e9b14c1cbd2cb9fa1d5
Mio Chung <miochung7@gmail.com>
2021-01-06 17:52:39 +0000

    Tested Capybara and Selenium

--- Gemfile
+++ Gemfile
@@ -2,6 +2,10 @@
 
 source "https://rubygems.org"
 
+set :session_secret, 'super secret'
+
 git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
 
 gem "sinatra"
+gem "capybara"
+gem "selenium-webdriver"
--- app.rb
+++ app.rb
@@ -1,5 +1,6 @@
 require 'sinatra'
 
+
 set :session_secret, 'super secret'
 
 get '/' do
@@ -15,8 +16,12 @@ get '/random-cat' do
   erb(:index)
 end
 
-get '/named-cat' do
+post '/named-cat' do
   p params
   @name = params[:name] 
   erb(:index)
+end
+
+get '/cat-form' do
+  erb(:cat_form)
 end
